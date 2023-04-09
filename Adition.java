d9ae3f38aa29f7f35c9347096d3b70e4ab4f8948
RAMESH <dhageramesh143@gmail.com>
2023-04-10 00:09:16 +0530

    second commit

---
+++ Adition.java
@@ -0,0 +1,14 @@
+
+public class Adition {
+	
+public static void main(String args[])
+{
+	String DHAGE="RAMESH";
+
+	StringBuffer ab=new StringBuffer(DHAGE);
+System.out.println(ab.reverse());
+
+  StringBuilder st=new StringBuilder(DHAGE);
+  
+	System.out.println(st.reverse());
+}}
